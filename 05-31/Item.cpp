166c1cb01cb4b3dd0614dbd80403e2993e35638a
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-02 20:23:08 -0700

    Update Item.cpp
    I implemented getAdjLocation(). Let me know if you see any errors

--- 05-31/Item.cpp
+++ 05-31/Item.cpp
@@ -50,6 +50,56 @@ int Item::getY()
 }
 std::vector<std::vector<int>> Item::getAdjLocation()
 {
+    /**
+     * each item will have either two, three, or four adjacent locations
+     *
+     * location0 is North (above current location)
+     * location1 is East (to the right of current location)
+     * location2 is South (under current location)
+     * location3 is West (to the left of current location)
+     * 
+     * top edge locations have location1, location2, location3
+     * right edge locations have location0, location2, location3
+     * bottom edge locations have location0, location1, location3
+     * left edge locations have location0, location1, location2
+     */
+    
+    int currentR = location[0];
+    int currentC = location[1];
+    
+    /* location0 */
+    adjLocation[0][0] = currentR-1;
+    adjLocation[0][1] = currentC;
+    
+    /* location1 */
+    adjLocation[1][0] = currentR;
+    adjLocation[1][1] = currentC+1;
+    
+    /* location2 */
+    adjLocation[2][0] = currentR+1;
+    adjLocation[2][1] = currentC;
+    
+    /* location3 */
+    adjLocation[3][0] = currentR;
+    adjLocation[3][1] = currentC-1;
+        
+    /**
+     * if the index is -1 or 20, the location is off the grid (current location is an edge or corner)
+     * and that location doesn't exist, so must be erased
+     */
+    
+    for(int i = 0; i<4; i++){
+        if(adjLocation[i][0] < 0 || adjLocation[i][0] > 19){
+            adjLocation.erase(adjLocation.begin()+i);
+            i--;
+        }
+        if(adjLocation[i][1] < 0 || adjLocation[i][1] > 19){
+            adjLocation.erase(adjLocation.begin()+i);
+            i--;
+        }
+    }
+        
     return adjLocation;
+    
 }
 
