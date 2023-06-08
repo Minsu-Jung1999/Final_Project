3ea3dfd5a799a615e61804a1f8978a511a0796d5
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-07 17:48:21 -0700

    Create Fire.cpp

---
+++ 06-04/Fire.cpp
@@ -0,0 +1,24 @@
+/**
+ * @brief Fire is red
+ * @details NONE
+ * @author Natalie Roberts, Minsu Jung
+ * @date June/7/2023
+ * @version NONE
+ */
+#include "Fire.h"
+#include <iostream>
+
+Fire::Fire(int x, int y): Item(x,y)
+{
+
+}
+
+std::string Fire::getColor()
+{
+    return "red";
+}
+
+std::string Fire::toString()
+{
+    return "F";
+}
