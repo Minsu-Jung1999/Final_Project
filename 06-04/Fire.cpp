bee3f20515383f5b602d219f56a3439a6cf02413
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-09 18:16:01 -0700

    Update Fire.cpp

--- 06-04/Fire.cpp
+++ 06-04/Fire.cpp
@@ -8,16 +8,21 @@
 #include "Fire.h"
 #include <iostream>
 
+
+// Constructs a Fire object with a location of an x and y value
+// Extends the Item superclass
 Fire::Fire(int x, int y): Item(x,y)
 {
 
 }
 
+// Overrides the Item function and sets Fire's color to red
 std::string Fire::getColor()
 {
     return "red";
 }
 
+// Overrides the Item function and makes Fire's toString return an F
 std::string Fire::toString()
 {
     return "F";
