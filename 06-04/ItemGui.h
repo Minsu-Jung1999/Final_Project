0e5719a5d272854c5e7e0fac061a936b2d276e5c
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-09 18:53:22 -0700

    Update ItemGui.h

--- 06-04/ItemGui.h
+++ 06-04/ItemGui.h
@@ -14,31 +14,34 @@
 #include "gbutton.h"
 #include "Item.h"
 
+/**
+ * @brief ItemGUI class creates an interactive gui window with buttons and a grid of colored squares
+ */
 class ItemGUI
 {
 private:
-    sgl::GWindow* window;
-    sgl::GButton* tickButton;
-    sgl::GButton* saveButton;
-    sgl::GButton* loadButton;
-    std::vector<std::vector<Item*>> grid;/* stores the classes in each grid */
+    sgl::GWindow* window; /* the gui window */
+    sgl::GButton* tickButton; /* a button to tick the simulation forward once */
+    sgl::GButton* saveButton; /* a button to save the current simulation to a file for later use */
+    sgl::GButton* loadButton; /* a button to load a previous simulation from a file */
+    std::vector<std::vector<Item*>> grid; /* stores the classes in each grid */
     const int rows;
     const int cols;
     const int width;
     const int height;
 
 public:
-    ItemGUI();
-    void draw();
-    void setting();
-    void drawGrid();
-    void openGUI();
-    void GridSetting();
-    void addButton();
-    void saveClassesToFile();
-    void loadClassesFromFile();
-    bool isWindowOpen();
-    void start();
+    ItemGUI(); 
+    void draw(); /* draws each shape */
+    void setting(); 
+    void drawGrid(); /* draws the entire grid */
+    void openGUI(); /* opens the gui */
+    void GridSetting(); 
+    void addButton(); /* adds a button */
+    void saveClassesToFile(); /* saves the current simulation to a txt file */
+    void loadClassesFromFile(); /* reads a previous simulation from a txt file */
+    bool isWindowOpen(); /* returns true if the gui is open */
+    void start(); /* places a fire square on the grid to begin the simulation */
 
 
 
