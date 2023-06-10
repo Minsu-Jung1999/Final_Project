082bca1b1d21f459073d5acc9f0a695f30e93537
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-09 19:23:22 -0700

    Update Water.cpp

--- 06-04/Water.cpp
+++ 06-04/Water.cpp
@@ -8,16 +8,21 @@
 #include "Water.h"
 #include <iostream>
 
+
+// Constructs a Water object which is blue and inflammable
+// Extends the Item superclass
 Water::Water(int x, int y): Item(x,y)
 {
 
 }
 
+// Overrides the Item function and sets Water's color to blue
 std::string Water::getColor()
 {
     return "blue";
 }
 
+// Overrides the Item function and makes Water's toString return a W
 std::string Water::toString()
 {
     return "W";
