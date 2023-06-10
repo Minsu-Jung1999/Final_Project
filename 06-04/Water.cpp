2e9f6951666b60d2a361120de12fe24b1d90f480
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-09 19:53:00 -0700

    Update Water.cpp

--- 06-04/Water.cpp
+++ 06-04/Water.cpp
@@ -1,6 +1,6 @@
 /**
- * @brief Water is blue
- * @details NONE
+ * @brief Water 
+ * @details Water is a subclass of item, but it is blue and inflammable
  * @author Natalie Roberts, Minsu Jung
  * @date May/15/2023
  * @version NONE
