8c9172ca7732d36d19861c07d515c79604da676c
nroberts1011 <134349810+nroberts1011@users.noreply.github.com>
2023-06-09 18:58:13 -0700

    Update Rock.cpp

--- 06-04/Rock.cpp
+++ 06-04/Rock.cpp
@@ -7,17 +7,20 @@
  */
 #include "Rock.h"
 
-
+// Constructs a Rock object with a location of an x and y value
+// Extends the Item superclass
 Rock::Rock(int x, int y) : Item(x,y)
 {
 
 }
 
+// Overrides the Item function and sets Rock's color to gray
 std::string Rock::getColor()
 {
     return "gray";
 }
 
+// Overrides the Item function and makes Rock's toString return an R
 std::string Rock::toString()
 {
     return "R";
